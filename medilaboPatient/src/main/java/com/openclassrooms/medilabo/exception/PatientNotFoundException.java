fff6d5efc15a6ba96a76f40a89936a9b28cbab80
Cyriac <drevia59@gmail.com>
2024-07-19 11:25:36 +0200

    reset

--- medilaboPatient/src/main/java/com/openclassrooms/medilabo/exception/PatientNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.openclassrooms.medilabo.exception;
-
-public class PatientNotFoundException extends Exception{
-
-    public PatientNotFoundException() {}
-    public PatientNotFoundException(String message) {
-        super(message);
-    }
-}
